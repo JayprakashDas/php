04fa0c3a794d2e82b9f3580d01bb11f332b4f1cd
JayprakashDas <47481071+JayprakashDas@users.noreply.github.com>
2019-09-09 17:46:37 +0530

    Update db.php

--- Blog_App/includes/db.php
+++ Blog_App/includes/db.php
@@ -1,9 +1,9 @@
 <?php 
 
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+$db['db_host'] = "gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db['db_user'] = "w6v5cku5v5o0kpjx";
+$db['db_pass'] = "az5anj8yi44pk4k8";
+$db['db_name'] = "unv7w3jplhr6odqn";
 
 foreach($db as $key => $value){
 	//here key is db and all this to make it constant
