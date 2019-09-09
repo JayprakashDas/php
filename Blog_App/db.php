8c6e7c5ad0a53e3576c102da1a8a971a4671b0fa
JayprakashDas <47481071+JayprakashDas@users.noreply.github.com>
2019-09-09 17:01:47 +0530

    Update db.php

--- Blog_App/db.php
+++ Blog_App/db.php
@@ -1,7 +1,7 @@
 
 
 <?php  
-$connection = mysqli_connect('localhost','root','','cms');
+$connection = mysqli_connect('gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','w6v5cku5v5o0kpjx','az5anj8yi44pk4k8','unv7w3jplhr6odqn');
 if($connection){
 	// echo "connected";
 }
